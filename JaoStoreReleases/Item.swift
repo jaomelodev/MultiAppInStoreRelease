d2ea801323f09a9f27dea7acad8d991a51099a92
João Mateus <joaomateusabm@yahoo.com.br>
2024-07-10 20:29:06 -0300

    Initial Commit

---
+++ JaoStoreReleases/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  JaoStoreReleases
+//
+//  Created by João Melo on 10/07/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
